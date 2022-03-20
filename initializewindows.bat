05ddcdd04ad4783c7ca4533f2192a36e225a1fa4
Dr_D00m4n <github@doomans.com>
2022-03-20 08:48:28 +0100

    nitial realease

---
+++ initializewindows.bat
@@ -0,0 +1,8 @@
+
+rem install github
+@REM winget install Git.Git
+rem install windows Terminal
+@REM winget install Microsoft.WindowsTerminal
+rem visual studio code
+@REM winget install Microsoft.VisualStudioCode
+git clone https://github.com/d00m4n/set-windowsMinimal.git
